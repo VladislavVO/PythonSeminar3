1cf1d07baaf8c3b18ad2cb8110a3dbde9d2d8935
Vladislav <vladislav.v.o@yandex.ru>
2022-09-30 19:01:32 +0300

    3rd

--- task3.py
+++ task3.py
@@ -5,14 +5,21 @@
 # [1.1, 1.2, 3.1, 5.17, 10.02] => 0.18 или 18
 # [4.07, 5.1, 8.2444, 6.98] - 0.91 или 91
 
-numbers = [1.1, 1.2, 3.1, 5.17, 10.02]
+numbers:int = [1.1, 1.2, 3.1, 5.17, 10.02]
 print (numbers)
 
-numbers2 = []
+numbers2= []
+numbers3= []
+numbers4= []
 for i in numbers:
     numbers2.append(int(str(i).split('.')[1]))
 
+    numbers3:int = numbers2
+    numbers4.append(numbers [i] - numbers [i])
+
 print (numbers2) 
+print (numbers3) 
+print (numbers4) 
 
 def find_max(numbers2):
     max=numbers(0)
